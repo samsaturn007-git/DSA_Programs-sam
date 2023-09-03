c0dfb4b67e5ec562bb61d7313c4198524a412624
samsaturn007-git <112267222+samsaturn007-git@users.noreply.github.com>
2023-09-03 14:28:14 +0530

    Add files via upload

---
+++ CircularQueue.c
@@ -0,0 +1,195 @@
+/*  Author: Vanessa jency   Branch: IT  Roll no.: 37
+    Circular queue
+*/
+
+#include<stdio.h>
+#define size 5
+
+int queue[size], front = -1, rear = -1;
+
+void enqueue();
+void dequeue();
+void isEmpty();
+void show();
+
+int main()
+{
+    int choice;
+    
+    while (1)
+    {
+        printf("\n*****Select a function to be performed*****");
+        printf("\n1.Enqueue       \n2.Dequeue       \n3.Is Empty       \n4.Display        \n5.Count        \n6.Exit");
+        printf("\nEnter your choice : ");
+        scanf("%d", &choice); //saves the user's choice
+
+        switch (choice)
+        {
+            case 1: enqueue();
+                    break;
+        
+            case 2: dequeue();
+                    break;
+    
+            case 3: isEmpty();
+                    break;
+    
+            case 4: show();
+                    break;
+
+            case 5: count();
+                    break;
+
+            case 6: return 0; 
+        
+            default:printf("\nInvalid choice!\n");
+
+        }
+
+    }
+    
+}
+
+void enqueue()
+{
+    int n;
+    
+    if (front==-1 && rear==-1) //checks if queue is empty
+        {
+            printf("\nEnter element to be added to the queue : ");
+            scanf("%d", &n);
+            front++;
+            queue[++rear]=n;
+        }
+
+    else 
+    {
+        
+        
+        if (front == (rear++) % size) //checks if queue is full
+        {
+            printf("\nQueue overflow!");
+        }
+
+        else
+        {
+            printf("\nEnter element to be added to the queue : ");
+            scanf("%d", &n);
+            rear = (rear++)%size;
+            queue[rear]=n;
+        }
+        
+    }
+    
+}
+
+void dequeue()
+{
+    if (front==-1 && rear==-1) //checks if queue is empty
+    {
+        printf("\nQueue underflow!");
+    }
+
+    else
+    {
+        if (front==rear) //checks if only one element is present in queue
+        {
+            front=-1;
+            rear=-1;
+        }
+
+        else
+        {
+            front=(front++)%size; 
+        }
+        
+    }
+
+}
+
+void isEmpty()
+{
+    if (front==-1 && rear==-1) //checks if queue is empty
+    {
+        printf("\nQueue is empty!");
+    }
+
+    else
+    {
+        printf("\nQueue is not empty!");
+    }
+
+}
+
+void show()
+{
+    int i, j;
+
+    if (front==-1 && rear==-1) //checks if queue is empty
+    {
+        printf("\nQueue is empty!");
+    }
+
+    else
+    {
+        for(i = front; i < size; i++)
+        {
+        	j = i%size;
+        	
+        	if(rear == front)
+        	{
+        		printf("\n%d", queue[j]);
+			}
+
+        	else if(i == front)
+        	{
+        		printf("\n%d",queue[j]);
+			}
+
+			else if(i == rear)
+			{
+				printf("\n%d",queue[j]);
+			} 
+
+			else
+			{
+            	printf("\n%d", queue[j]);
+        	}
+
+        }
+
+    }
+    
+}
+
+void count()
+{
+    int i, j, count;
+
+    if (front==-1 && rear==-1) //checks if queue is empty
+    {
+        printf("\nQueue is empty!");
+    }
+    else{
+        for(i = front; i < size; i++){      	
+        	if(rear == front)
+        	{
+        		count++;
+			}
+        	else if(i == front)
+        	{
+        		count++;
+			}
+			else if(i == rear)
+			{
+				count++;
+			} 
+			else
+			{
+            	count++;
+        	}
+        }
+        printf("There are %d elements in queue.",count);
+    }
+    
+}
---
+++ queue1.c
@@ -0,0 +1,151 @@
+/* header files */
+#include<stdio.h>
+#include<stdlib.h>
+
+/* defining 6 as size */
+#define size 6
+
+/* Queue initialization */
+int queue[size];
+int f = -1;
+int r = -1;
+
+/* function prototypes */
+void enqueue(int);
+void dequeue();
+void isEmpty();
+void show();
+
+/* main function */
+int main()
+{
+	// declaration of variables //
+	int ch, n;
+	/* while loop */
+	while(1)
+	{
+		// Choice Display //
+		printf("\n----Queue Menu----");
+		printf("\n1. Enqueue");
+		printf("\n2. Dequeue");
+		printf("\n3. Check Empty");
+		printf("\n4. Display Queue");
+		printf("\n5. Exit");
+		printf("\nEnter your choice : ");
+		scanf("%d", &ch);
+		
+		// switch case // 
+		switch(ch)
+		{
+			case 1:
+				printf("\nEnter a Integer value : ");
+				scanf("%d", &n);
+				enqueue(n); break;
+			case 2:
+				dequeue();break;
+			case 3:
+				isEmpty();break;
+			case 4:
+				show();break;
+			case 5:
+				exit(0);break;
+			default:
+				printf("\nInvalid Choice! Try Again!");
+		}
+		// switch case ends here //
+
+	}
+	/* while loop ends here */
+	return 0;
+}
+/* main function ends here */
+
+/* enqueue() : adds element to queue*/
+void enqueue(int n)
+{
+	if(f == -1 && r == -1)
+	{
+		f++;
+		queue[++r] = n;
+	} 
+	else
+	{
+		if()
+		{
+			printf("\nqueue is full!");
+		}	
+		else
+		{
+			queue[++r] = n;
+		}
+	}
+}
+/* enqueue() function ends here */
+
+/* dequeue() : deletes element from front end of queue*/
+void dequeue()
+{
+	if(f == -1 && r == -1)
+	{
+		printf("\nQueue is Empty!");
+	}
+	else if(f == r)
+	{
+		f = -1;
+		r = -1;
+	}
+	else
+	{
+		f++;
+	}
+}
+/* dequeue ends here */
+
+/* isEmpty() : check is the queue is empty or not */
+void isEmpty()
+{
+	if(f == -1 && r == -1)
+	{
+		printf("\nQueue is Empty!");
+	}
+	else
+	{
+		printf("\nQueue is not Empty!");
+	}
+}
+/* isEmpty func ends here */
+
+/* show() : displays queue with front and rear pointers */
+void show()
+{
+	int i;
+	if(f == -1 && r == -1)
+	{
+		printf("\nQueue is Empty!");
+	} 
+	else
+	{
+		printf("\n------------");
+		for(i = f; i <= r; i++)
+		{
+			if(i == f && i == r)
+			{
+				printf("\n%d <- front and rear", queue[i]);
+			}
+			else if(i == f)
+			{
+				printf("\n%d <- front", queue[i]);
+			}
+			else if(i == r)
+			{
+				printf("\n%d <- rear", queue[i]);
+			}
+			else
+			{
+				printf("\n%d", queue[i]);
+			}
+		}
+		printf("\n------------");
+	}
+}
+/* show function ends here */
